d4b74c7b22152c0d67861820d86f2518082aed46
Kez Anwar <kezanwar@gmail.com>
2024-08-31 16:13:26 +0100

    frontend/added made request base url static

--- frontend/src/services/request.ts
+++ frontend/src/services/request.ts
@@ -1,7 +1,28 @@
-import { BASE_URL } from './request';
 import axios, { AxiosError, AxiosProgressEvent } from 'axios';
 
-const axiosInstance = axios.create({ baseURL: BASE_URL });
+type OnUploadProgress = (progress: AxiosProgressEvent) => void;
+
+class Request {
+  static BASE_URL: string = import.meta.env.VITE_BASE_URL || 'no-baseurl-found';
+
+  static postFileBinary(data: FormData, onProgress: OnUploadProgress) {
+    axiosInstance.post(`${this.BASE_URL}/upload`, data, {
+      onUploadProgress: onProgress
+    });
+  }
+
+  static getFileBinary(uuid: string, onProgress: OnUploadProgress) {
+    axiosInstance.get(`${this.BASE_URL}/file/${uuid}`, {
+      onDownloadProgress: onProgress
+    });
+  }
+
+  static getFilesMeta(uuids: string[]) {
+    axiosInstance.get(`${this.BASE_URL}/files/meta`, { params: { uuids } });
+  }
+}
+
+const axiosInstance = axios.create({ baseURL: Request.BASE_URL });
 
 axiosInstance.interceptors.response.use(
   (response) => response,
@@ -13,27 +34,5 @@ axiosInstance.interceptors.response.use(
     Promise.reject(err);
   }
 );
-
-type OnUploadProgress = (progress: AxiosProgressEvent) => void;
-
-class Request {
-  static BASE_URL: string = import.meta.env.VITE_BASE_URL || 'no-baseurl-found';
-
-  static postFileBinary(data: FormData, onProgress: OnUploadProgress) {
-    axios.post(`${this.BASE_URL}/upload`, data, {
-      onUploadProgress: onProgress
-    });
-  }
-
-  static getFileBinary(uuid: string, onProgress: OnUploadProgress) {
-    axios.get(`${this.BASE_URL}/file/${uuid}`, {
-      onDownloadProgress: onProgress
-    });
-  }
-
-  static getFilesMeta(uuids: string[]) {
-    axios.get(`${this.BASE_URL}/files/meta`, { params: { uuids } });
-  }
-}
 
 export default Request;
