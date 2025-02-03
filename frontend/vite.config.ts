1e61eac8147c7eda41c993d5738436e1c33f5113
Kez Anwar <kezanwar@gmail.com>
2025-02-03 23:02:45 +0000

    fe/added vite updates for building

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'node:path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
-    alias: [{ find: '@app', replacement: '/src' }]
+    alias: { '@app': path.resolve(__dirname, 'src') }
   },
   base: './',
   plugins: [react()]
